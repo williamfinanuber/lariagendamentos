6d3670ee52c9eb2f7fa32541f7e714525c379fb3
Jackson Pereira dos Santos <jacksonpereirad@gmail.com>
2025-11-13 15:14:15 +0000

    No caso esta dando conflito no versel posi ele da erro dizendo que tem r

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,6 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
-  },
   eslint: {
     ignoreDuringBuilds: true,
   },
